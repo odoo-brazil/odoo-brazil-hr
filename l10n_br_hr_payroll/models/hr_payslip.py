5a4c08124f4646de51e7a95e5fa33fe3d169b306
Hendrix Costa <hendrixcosta@gmail.com>
2018-09-24 09:10:07 -0300

    [FIX] PEP8

--- l10n_br_hr_payroll/models/hr_payslip.py
+++ l10n_br_hr_payroll/models/hr_payslip.py
@@ -1018,7 +1018,8 @@ class HrPayslip(models.Model):
 
             # Seta a Data final do aquisitivo apartir da inicial, para sempre
             # pegar os holerites validos do periodo cheio
-            dt_fim = fields.Datetime.from_string(periodo.inicio_aquisitivo) + relativedelta(years=1, days=-1)
+            dt_fim = fields.Datetime.from_string(periodo.inicio_aquisitivo) + \
+                     relativedelta(years=1, days=-1)
 
             if periodo.saldo != 0:
                 #
