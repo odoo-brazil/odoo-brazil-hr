45dbb7ba2a11bcfcecf0a8373ebe28b1a348f33c
Hendrix Costa <hendrix.costa@abgf.gov.br>
2019-02-12 13:25:03 -0200

    [FIX] Renomeando arquivos

---
+++ l10n_br_hr_payroll_account/models/account_move_line.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2016 KMEE (http://www.kmee.com.br)
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+from openerp import models, fields, api
+
+
+class AccountMove(models.Model):
+    _inherit = "account.move"
+
+    payslip_id = fields.Many2one(
+        comodel_name="hr.payslip",
+        string="Payslip",
+        ondelete='cascade',
+    )
+
+    payslip_autonomo_id = fields.Many2one(
+        comodel_name="hr.payslip.autonomo",
+        string="Payslip Autonomo",
+        ondelete='cascade',
+    )
+
+    payslip_run_id = fields.Many2one(
+        comodel_name="hr.payslip.run",
+        string="Payslip Run",
+        ondelete='cascade',
+    )
+
+    @api.multi
+    def name_get(self):
+        res = []
+        for record in self:
+            if record.payslip_id or record.payslip_run_id:
+                name = record.name
+                res.append((record.id, name))
+
+            if res:
+                return res
+            else:
+                return super(AccountMove, self).name_get()
+
+
+class AccountMoveLine(models.Model):
+    _inherit = "account.move.line"
+
+    payslip_id = fields.Many2one(
+        comodel_name="hr.payslip",
+        string="Payslip",
+    )
+
+    payslip_autonomo_id = fields.Many2one(
+        comodel_name="hr.payslip.autonomo",
+        string="Payslip Autonomo",
+        ondelete='cascade',
+    )
+
+    payslip_run_id = fields.Many2one(
+        comodel_name="hr.payslip.run",
+        string="Payslip Run",
+    )
