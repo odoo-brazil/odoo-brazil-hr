35cd8e0409cbd984e9d21b4ef37b62513e5a44ad
Hendrix Costa <hendrix.costa@abgf.gov.br>
2019-02-12 16:29:52 -0200

    [FIX] Separar modelos

--- l10n_br_hr_payroll_account/models/account_move_line.py
+++ l10n_br_hr_payroll_account/models/account_move_line.py
@@ -1,51 +1,21 @@
 # -*- coding: utf-8 -*-
 # Copyright (C) 2016 KMEE (http://www.kmee.com.br)
+# Copyright (C) 2018 ABGF (http://www.abgf.gov.br)
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from openerp import models, fields, api
 
+from __future__ import absolute_import, print_function, unicode_literals
 
-class AccountMove(models.Model):
-    _inherit = "account.move"
-
-    payslip_id = fields.Many2one(
-        comodel_name="hr.payslip",
-        string="Payslip",
-        ondelete='cascade',
-    )
-
-    payslip_autonomo_id = fields.Many2one(
-        comodel_name="hr.payslip.autonomo",
-        string="Payslip Autonomo",
-        ondelete='cascade',
-    )
-
-    payslip_run_id = fields.Many2one(
-        comodel_name="hr.payslip.run",
-        string="Payslip Run",
-        ondelete='cascade',
-    )
-
-    @api.multi
-    def name_get(self):
-        res = []
-        for record in self:
-            if record.payslip_id or record.payslip_run_id:
-                name = record.name
-                res.append((record.id, name))
-
-            if res:
-                return res
-            else:
-                return super(AccountMove, self).name_get()
+from openerp import models, fields
 
 
 class AccountMoveLine(models.Model):
-    _inherit = "account.move.line"
+    _inherit = b'account.move.line'
 
     payslip_id = fields.Many2one(
         comodel_name="hr.payslip",
         string="Payslip",
+        ondelete='cascade',
     )
 
     payslip_autonomo_id = fields.Many2one(
@@ -57,4 +27,5 @@ class AccountMoveLine(models.Model):
     payslip_run_id = fields.Many2one(
         comodel_name="hr.payslip.run",
         string="Payslip Run",
+        ondelete='cascade',
     )
